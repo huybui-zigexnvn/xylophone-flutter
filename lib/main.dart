c52ee4b1c0db4f27089512ef2e56a12837f6d88f
huybq-zigexnvn <huybq@zigexn.vn>
2024-07-10 09:33:36 +0700

    Finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
+  List<Widget> keyBoard() {
+    var mapKeys = {
+      Colors.red: 1,
+      Colors.orange: 2,
+      Colors.yellow: 3,
+      Colors.green: 4,
+      Colors.teal: 5,
+      Colors.blue: 6,
+      Colors.purple: 7
+    };
+    return mapKeys.entries.map((entry) {
+      // entry.key là tên màu, entry.value là số tương ứng
+      return Expanded(
+        child: TextButton(
+          onPressed: () {
+            PlayMusic(entry.value);
+          },
+          child: Container(
+            color: entry.key,
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  void PlayMusic(int number) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$number.wav'));
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Container(
+                child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: keyBoard(),
+        ))),
       ),
     );
   }
